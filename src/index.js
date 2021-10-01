eec578b1271677725dbf4927d0f6e87eea171f62
Dvornick <mail@dvornick.ru>
2021-10-01 21:42:10 +0300

    Displaying the platform and the ball

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 class Game {
-    start() {
+    constructor() {
+        this.ctx = null
+        this.sprites = {
+            background: null,
+            ball: null,
+            platform: null
+        }
+    }
+
+    init () {
+        // Init
         this.ctx = document.getElementById('myCanvas').getContext('2d')
-        const background = new Image()
-        background.src = './img/background.png'
+    }
+
+    preload(callback) {
+        let loaded = 0
+        let required = Object.keys(this.sprites).length
+
+        for(let key in this.sprites) {
+            this.sprites[key] = new Image()
+            this.sprites[key].src = `./img/${key}.png`
+            this.sprites[key].addEventListener('load', () => {
+                ++loaded
+                if (loaded >= required) {
+                    callback()
+                }
+            })
+        }
+    }
+
+    run() {
         window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0)
+            this.render()
+        })
+    }
+
+    render() {
+        this.ctx.drawImage(this.sprites.background, 0, 0)
+        this.ctx.drawImage(this.sprites.ball, 0, 0)
+        this.ctx.drawImage(this.sprites.platform, 0, 0)
+    }
+    start() {
+        this.init()
+        this.preload(() => {
+            this.run()
         })
     }
 }
