be6d6665c5fe7ce05e9c05a81621e62be2949b22
Dvornick <mail@dvornick.ru>
2021-10-02 16:45:37 +0300

    Displaying blocks on the screen

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ class Game {
         this.ctx = null
         this.platform = null
         this.ball = null
+        this.blocks = []
+        this.rows = 4
+        this.cols = 8
         this.sprites = {
             background: null,
             ball: null,
-            platform: null
+            platform: null,
+            block: null
         }
     }
 
@@ -18,16 +22,27 @@ class Game {
     preload(callback) {
         let loaded = 0
         let required = Object.keys(this.sprites).length
-
+        let onImageLoad = () => {
+            ++loaded
+            if (loaded >= required) {
+                callback()
+            }
+        }
         for(let key in this.sprites) {
             this.sprites[key] = new Image()
             this.sprites[key].src = `./img/${key}.png`
-            this.sprites[key].addEventListener('load', () => {
-                ++loaded
-                if (loaded >= required) {
-                    callback()
-                }
-            })
+            this.sprites[key].addEventListener('load', onImageLoad)
+        }
+    }
+
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 23 * row + 35
+                })
+            }
         }
     }
 
@@ -41,10 +56,14 @@ class Game {
         this.ctx.drawImage(this.sprites.background, 0, 0)
         this.ctx.drawImage(this.sprites.ball, 0, 0, 20, 20, this.ball.x, this.ball.y, this.ball.width, this.ball.height)
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y)
+        for (let block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y)
+        }
     }
     start() {
         this.init()
         this.preload(() => {
+            this.create()
             this.run()
         })
     }
